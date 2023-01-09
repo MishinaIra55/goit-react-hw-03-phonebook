06e33bd9e5ca64ecec6756613fce9ec04a136582
MishynaIra <gortenzia1987@gmail.com>
2023-01-09 14:33:08 +0200

    записывает и удаляет с localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,12 +13,28 @@ class App extends Component {
   state = {
     filter: '',
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(localContacts);
+
+    if (parseContacts) {
+      this.setState( { localContacts: parseContacts});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    if (this.state.contacts !==prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
 //новый контакт для добавления
   addForm = data => {
